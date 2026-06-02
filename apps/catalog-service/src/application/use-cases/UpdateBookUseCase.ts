f94305d2367afe03b7939f1e723a48d0ee80bedf
kleberchiliquinga <kachiliquingal@uce.edu.ec>
2026-06-01 21:38:51 -0500

    feat(catalog): add update and delete use cases to complete domain CRUD

---
+++ apps/catalog-service/src/application/use-cases/UpdateBookUseCase.ts
@@ -0,0 +1,22 @@
+import { Book } from "../../domain/entities/Book";
+import { BookRepository } from "../../domain/ports/BookRepository";
+
+export class UpdateBookUseCase {
+  constructor(private readonly bookRepository: BookRepository) {}
+
+  async execute(id: string, bookData: Partial<Book>): Promise<Book> {
+    // Here we could add business rules, for example:
+    // Do not allow changes to the ISBN once it has been created.
+    if (bookData.isbn) {
+      throw new Error("ISBN cannot be modified after creation");
+    }
+
+    const updatedBook = await this.bookRepository.update(id, bookData);
+
+    if (!updatedBook) {
+      throw new Error(`Book with id ${id} not found in the catalog`);
+    }
+
+    return updatedBook;
+  }
+}
--- apps/catalog-service/src/domain/ports/BookRepository.ts
+++ apps/catalog-service/src/domain/ports/BookRepository.ts
@@ -4,4 +4,6 @@ export interface BookRepository {
   save(book: Book): Promise<Book>;
   findAll(): Promise<Book[]>;
   findByIsbn(isbn: string): Promise<Book | null>;
+  update(id: string, book: Partial<Book>): Promise<Book | null>;
+  delete(id: string): Promise<boolean>;
 }
