aefae0a92e48d8552da7b7575d15b1ec55756453
kleberchiliquinga <kachiliquingal@uce.edu.ec>
2026-06-15 22:16:33 -0500

    fix(infra): add missing ssh key_name to production instances

--- infrastructure/prod/api_gateway.tf
+++ infrastructure/prod/api_gateway.tf
@@ -5,6 +5,7 @@
 resource "aws_instance" "api_gateway_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name      = var.aws_key_name
 
   vpc_security_group_ids = [aws_security_group.api_gateway_sg.id]
 
