4c656fa7ccefa6badadc66228d8fb19e072b04f7
kleberchiliquinga <kachiliquingal@uce.edu.ec>
2026-06-19 17:14:03 -0500

    feat(infra): add dedicated stateful database_server for QA

--- infrastructure/qa/outputs.tf
+++ infrastructure/qa/outputs.tf
@@ -16,3 +16,13 @@ output "brokers_public_ip" {
   description = "The public IP of the Brokers (Kafka, RabbitMQ, MQTT) Server"
   value       = aws_instance.brokers_server.public_ip
 }
+
+output "database_private_ip" {
+  description = "The private IP of the Database Server"
+  value       = aws_instance.database_server.private_ip
+}
+
+output "database_public_ip" {
+  description = "The public IP of the Database Server"
+  value       = aws_instance.database_server.public_ip
+}
