9ef0618b2a696b604042bea978ab4267a38b45c7
kleberchiliquinga <kachiliquingal@uce.edu.ec>
2026-07-04 11:34:19 -0500

    feat(infra): add inventory-service to api gateway routing

--- infrastructure/prod/api_gateway.tf
+++ infrastructure/prod/api_gateway.tf
@@ -32,6 +32,7 @@ docker run -d -p 80:80 --name uce-api-gateway \
   -e SEARCH_SERVICE_URL=${aws_lb.main.dns_name}:80 \
   -e FINE_SERVICE_URL=${aws_instance.fine_server.private_ip}:3006 \
   -e NOTIFICATION_SERVICE_URL=${aws_instance.notification_server.private_ip}:3005 \
+  -e INVENTORY_SERVICE_URL=${aws_instance.inventory_server.private_ip}:4009 \
   -e BROKERS_IP=${aws_instance.brokers_server.private_ip} \
   --restart always $IMAGE_NAME
 
